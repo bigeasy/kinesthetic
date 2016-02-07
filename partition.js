6e31a2ab979d52916a293aa8e04112848d92440e
Alan Gutierrez <alan@prettyrobots.com>
2016-02-06 23:50:29 -0600

    `partition` returns ranges.

--- partition.js
+++ partition.js
@@ -4,11 +4,12 @@ function partition (shards) {
     var n = new Integer('ffffffffffffffffffffffffffffffff', 16)
     var size = n.divide(shards)
     var partition = new Integer(0)
-    var partitions = []
+    var partitions = [{ from: 0, to: partition.add(size).subtract(1) }]
     for (var i = 1; i < shards; i++) {
         partition = partition.add(size)
-        partitions.push(partition.add(0))
+        partitions.push({ from: partition.add(0), to: partition.add(size).subtract(1) })
     }
+    partitions[partitions.length - 1].to = n
     return partitions
 }
 
--- t/kinesthetic/partition.t.js
+++ t/kinesthetic/partition.t.js
@@ -3,12 +3,22 @@ require('proof')(1, prove)
 function prove (assert) {
     var partition = require('../../partition')
     assert(partition(6).map(function (partition) {
-        return partition.toString(16)
+        return {
+            from: partition.from.toString(16),
+            to: partition.to.toString(16)
+        }
     }), [
-        '2aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-        '55555555555555555555555555555554',
-        '7ffffffffffffffffffffffffffffffe',
-        'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa8',
-        'd5555555555555555555555555555552'
+        { from: '0',
+            to: '2aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa9' },
+        { from: '2aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+            to: '55555555555555555555555555555553' },
+        { from: '55555555555555555555555555555554',
+            to: '7ffffffffffffffffffffffffffffffd' },
+        { from: '7ffffffffffffffffffffffffffffffe',
+            to: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa7' },
+        { from: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa8',
+            to: 'd5555555555555555555555555555551' },
+        { from: 'd5555555555555555555555555555552',
+            to: 'ffffffffffffffffffffffffffffffff' }
     ], 'partition')
 }
